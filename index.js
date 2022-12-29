96e262c87cdd2f3356ee30ad67c2d34d45d564b8
NeedleKO <marksjagurs@gmail.com>
2022-12-30 00:56:46 +0200

    paturpināju js, daudz bagu un nepilnību gan vēl

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
+const buttonsAll = document.querySelectorAll("button")
+let display = document.querySelector(".display")
+operators = ["*", "/", "-", "+"]
+
+
+
+
+buttonsAll.forEach((e) => {
+    e.addEventListener("click", (e) => {
+        let clicked = e.target.id
+        console.log(clicked)
+        let displayContent = display.innerHTML += clicked
+        
+        if (clicked === "=") {
+            if (displayContent.includes("/")) {
+                const splitStuff = displayContent.split(" /")
+                console.log(splitStuff)
+            }
+            if (displayContent.includes("-")) {
+                
+            }
+            if (displayContent.includes("+")) {
+                
+            }
+            if (displayContent.includes("*")) {
+                
+            }
+        }
+
+
+
+        /*
+        if (clicked === "CLEAR") {
+            location.reload();
+        }
+        */
+        
+    })
+})
+
+const operate = (operator, num1, num2) => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function add(num1, num2) {
     return num1 + num2
 }
@@ -16,7 +74,22 @@ function divide(num1, num2) {
 }
 
 
-function operate(operator, num1, num2) {
-    
+
+
+function operators(numbers) {
+    return numbers
 }
+
+
+
+
+console.log(operators(subtract(5,5)))
+
+
+*/
+
+
+
+
+
 
